f4b66a8d03ec8c8dc4e5eb3c0bde372e5ec081f3
Farouk Hamadi <faroukhamadi1@outlook.com>
2022-03-02 16:35:14 +0100

    requires high IQ

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+struct Node
+{
     int value;
     struct Node* next;
 };typedef struct Node Node;
 
 Node* head;
 
+void inputArraySize(int* n) {
+    do {
+        printf("Enter the linked list size: \n");
+        scanf("%d", n);
+    } while (*n > 10);
+}
 
 void createLinkedList(int n) {
     int x;
@@ -36,40 +43,81 @@ void printLinkedList() {
         printf("%d -> ", current->value);
         current = current->next;
     }
+
     printf("NULL");
 }
 
+void countLengthLinkedList() {
+    int length = 0;
+    Node* current = head;
 
-void inputArraySize(int* n) {
-    do {
-        printf("Enter the Linked List size: \n");
-        scanf("%d", n);
-    } while (*n > 6);
+    while (current) {
+        length++;
+        current = current->next;
+    }
+
+    printf("\nThe length of the Linked List is : %d\n", length);
+    
 }
 
+void countOccurencesLinkedList(int value) {
+    int occurrences = 0;
+    Node* current = head;
+
+    while (current) {
+        if (current->value == value) {
+            occurrences++;
+        }
+        current = current->next;
+    }
+    printf("\nThe number of occurences of the Linked List : %d\n", occurrences);
+}
+
+int getIndexOfNode(int value) {
+    Node* current = head;
+    int index = 0;
+
+    while (current) {
+        if (current->value == value) {
+            return index;
+        }
+        index++, current = current->next;
+    }
+    return -1;
+}
+
+void deleteElementLinkedList(int value) {
+    Node* current = head;
+    int deleteIndex = getIndexOfNode(value);
+    int index = 0;
+
+    for (size_t i = 0; i < deleteIndex - 1; i++) {
+        current = current->next;
+    }
+
+    Node* current1 = current->next;
+    current->next = current1->next;
+    free(current1);
+    
+}
 
 int main() {
-    // Node* head = malloc(sizeof(Node));
-    // Node* second = malloc(sizeof(Node));
-    // Node* third = malloc(sizeof(Node));
 
-    // head->value = 69;
-    // second->value = 79;
-    // third->value = 89;
-
-    // head->next = second;
-    // second->next = third;
-    // third->next = NULL;
     int n;
 
     inputArraySize(&n);
 
-    printf("n = %d", n);
-    
     createLinkedList(n);
-
+    
     printLinkedList();
 
+    countLengthLinkedList();
+
+    countOccurencesLinkedList(3);
+
+    deleteElementLinkedList(3);
+        
+    printLinkedList();
     
     return 0;
 }
